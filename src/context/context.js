f183b0c82b0601ca2fc0d2be1773681914997ca7
Yuliya Kasavets <kosoveculia8@gmail.com>
2026-04-08 23:23:50 +0300

    added routes

---
+++ src/context/context.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+const GitHubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  return <GitHubContext.Provider></GitHubContext.Provider>;
+};
